3ab5222d60572ee61577aeda511fc24b3d450a7c
Giulia <giuliafloris88@gmail.com>
2022-06-22 09:49:55 +0200

    Initial Commit

---
+++ DataFetch2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DataFetch2
+//
+//  Created by user on 22/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DataFetch2/DataFetch2App.swift
@@ -0,0 +1,17 @@
+//
+//  DataFetch2App.swift
+//  DataFetch2
+//
+//  Created by user on 22/06/22.
+//
+
+import SwiftUI
+
+@main
+struct DataFetch2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
